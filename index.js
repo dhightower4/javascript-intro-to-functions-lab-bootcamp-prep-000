01a02d5d502ed0286bcfb259d18e812aaf36a7d5
Donte Hightower <dontehightower12@gmail.com>
2017-08-29 22:53:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+  console.log ('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase === uppercase
+
+var lowercase = "hello!"
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = "I love you grandma"
+
+mixedCase.toLowerCase === lowercase
+
+mixedCase.toUpperCase === uppercase
+
+function sayHiToGrandma(string) {
+    if (string.toLowerCase === string) {
+      return("I can't hear you!")
+    }
+}
