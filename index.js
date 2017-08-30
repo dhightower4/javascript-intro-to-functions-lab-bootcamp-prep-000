1d00eb743d1edfa6118b6a285ffbe43345022e4b
Donte Hightower <dontehightower12@gmail.com>
2017-08-30 00:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
       return("I can\'t hear you!");
     } else if (string === string.toUpperCase()) {
       return("YES INDEED!");
-    } else if (string === string.mixedCase()) {
+    } else {
       return("I love you, too.")
     }
 }
